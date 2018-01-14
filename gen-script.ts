0001bcde8ce1adda5aa06442e7930a4f8ebad048
Michael North <michael.l.north@gmail.com>
2018-01-13 22:21:12 -0800

    Emit script

--- gen-script.ts
+++ gen-script.ts
@@ -1,19 +1,26 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import * as https from 'https';
+
+const S3 = new AWS.S3();
 
 const genScript: Handler = (
   event: APIGatewayEvent,
   context: Context,
   cb: Callback
 ) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message:
-        'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event
-    })
-  };
-
-  cb(null, response);
+  S3.getObject(
+    {
+      Bucket: 'mikeworks-libs',
+      Key: 'techcheck/index.js'
+    },
+    (err, data) => {
+      const response = {
+        statusCode: 200,
+        body: data.Body.toString()
+      };
+      cb(null, response);
+    }
+  );
 };
 export default genScript;
