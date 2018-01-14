390a11c2be0dcb2fd9109f90db27d1aba10ee87c
Michael North <michael.l.north@gmail.com>
2018-01-13 21:29:41 -0800

    initial commit

---
+++ gen-script.ts
@@ -0,0 +1,19 @@
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+
+const genScript: Handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  cb: Callback
+) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event
+    })
+  };
+
+  cb(null, response);
+};
+export default genScript;
